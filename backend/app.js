093f29c38cc92ca11c6cfd5bdc50d346610a359e
Orkea <orkea@hotmail.fr>
2024-06-14 17:31:47 +0200

    Ajout des routes put et delete + ajout multer

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
+const mongoose = require('mongoose')
 // Installation du framework Express
 const express = require('express')
-const app = express()
-// Importation du routeur
+// Importation por accéder au path du serveur
+const path = require('path')
+// Importation du routeur sauce
+const sauceRoutes = require('./routes/sauce')
+// Importation du routeur utilisateurs
 const userRoutes = require('./routes/user')
 
+const app = express()
 // Installation de mongoose et connexion
-const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://orkea971:rDIi3kJn3e0cNFmX@orkea-p6.me8lzrt.mongodb.net/?retryWrites=true&w=majority&appName=Orkea-P6')
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'))
@@ -19,26 +23,14 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
-})  
-
-//Enregistrement du router
-app.use('/api/auth', userRoutes)
-
-//Configurons une réponse simple
-app.get('/api/sauce',(req, res, next) => {
-
-const sauce = [
-    {
-        userId: "MondMongoDB Id",
-        name: "Sauce piment",
-        manufacturer : "Gwada sauce",
-        description : "Sauce piquant*5",
-        mainPepper : "BondaManJak",
-    }
-]    
-res.status(200).json(sauce)
-console.log(sauce)
 })
+
+// Enregistrement du router utilisateur
+app.use('/api/auth', userRoutes)
+// Enregistrement du router sauce
+app.use('/api/sauces', sauceRoutes)
+// Gestion de la ressource images de maniére statique
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 
 module.exports = app
---
+++ backend/routes/sauce.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+// Importation du middleware d'authentification
+const auth = require('../middleware/auth')
+// Importation du middleware multer pour les images
+const multer =  require('../middleware/multer-config')
+// Importation du controleur pour les sauces
+const sauceCtrl = require('../controllers/sauce')
+
+//Implementation des routers 
+router.get('/', auth, sauceCtrl.getAllSauce)
+router.post('/', auth, multer, sauceCtrl.createSauce)
+router.get('/:id', auth, sauceCtrl.getOneSauce)
+router.put('/:id', auth, multer, sauceCtrl.modifySauce)
+router.delete('/:id', auth, sauceCtrl.deleteSauce)
+
+module.exports = router
